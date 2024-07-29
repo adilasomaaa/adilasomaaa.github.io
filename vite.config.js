8fb7f0853ef67f5cd5f6d13455deba2a34b2355c
Yasdil Lasoma <yasdilofficial@gmail.com>
2024-07-29 22:32:02 +0800

    tes

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,35 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue(), 
+    VitePWA(
+      { 
+        registerType: 'autoUpdate',
+        manifest: {
+          name: 'My Awesome App',
+          short_name: 'MyApp',
+          description: 'My Awesome App description',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: 'android-chrome-192x192.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: 'android-chrome-512x512.png',
+              sizes: '512x512',
+              type: 'image/png'
+            }
+          ]
+        }
+      }
+    )
   ],
   resolve: {
     alias: {
